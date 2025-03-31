ceccfe2ccb7ee1e686927cddb1db50f4de00c197
fursew <uirk00817@gmail.com>
2025-03-31 14:57:54 +0900

    web_update

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -7,7 +7,7 @@
   const ctx = document.getElementById('myChart')
   // eslint-disable-next-line no-unused-vars
   const myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
       // x축 데이터터
       labels: {{x_data | tojson}},
@@ -15,36 +15,6 @@
       datasets: [{
         // data -> y축 데이터터
         data: {{y_data | tojson}},
-        lineTension: 0,
-        backgroundColor: 'transparent',
-        borderColor: '#007bff',
-        borderWidth: 4,
-        pointBackgroundColor: '#007bff'
-      }]
-    },
-    options: {
-      plugins: {
-        legend: {
-          display: false
-        },
-        tooltip: {
-          boxPadding: 3
-        }
-      }
-    }
-  })
-})
-const ctx2 = document.getElementById('myChart')
-  // eslint-disable-next-line no-unused-vars
-  const myChart2 = new Chart(ctx2, {
-    type: 'bar',
-    data: {
-      // x축 데이터터
-      labels: {{x_data | tojson}},
-      // y축의 데이터들들
-      datasets: [{
-        // data -> y축 데이터터
-        data: {{y1_data | tojson}},
         lineTension: 0,
         backgroundColor: 'transparent',
         borderColor: '#007bff',
